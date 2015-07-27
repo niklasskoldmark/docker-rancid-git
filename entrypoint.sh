48465025373a782336981eefe48c06854c11363a
niklasskoldmark <niklas.skoldmark@gmail.com>
2015-07-27 22:26:11 +0200

    Initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+#################################################################
+# Setup postfix
+postconf -e relayhost="$POSTFIX_RELAY_PORT_25_TCP_ADDR"
+#################################################################
 # Run rancid-cvs to check for configuration changes
 /var/lib/rancid/bin/rancid-cvs
+#################################################################
 # Run rancid-run
 /var/lib/rancid/bin/rancid-run
